3fc167e8ce960f3b8251189035ee930f589a3f16
sneha Anil Memane <114849578+snehanil95@users.noreply.github.com>
2023-01-30 16:10:23 +0530

    Delete Que3.py

--- Que3.py
+++
@@ -1,8 +0,0 @@
-#1.reverse -i-search use for command history
-#2.ls -a
-#3.from college time i m using linux so,i m very friendly/comfortable with linux
-#and its with CUI(command user interface)
-
-#4.apache tomcat for servlet application running
- #and SMTP for creating gmail automation script(or in general email sending)
-
